ddbcbfd52fe30cbc6efd9d54d81dac9c3b1de040
bichi <tomasborghi13@gmail.com>
2025-03-30 09:35:30 -0300

    working in vscode

--- ex-1.c
+++ ex-1.c
@@ -1,6 +1,8 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
+void printNumbers();
+
 int main() {
     
     char nombre[] = "Tomas";
@@ -10,5 +12,24 @@ int main() {
     printf("%s\n", nombre);
     printf("%s\n", apellido);
     printf("%d\n", edad);
+
+    printNumbers();
+    
     return 0;
 }
+
+void printNumbers() {
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+    printf("6\n");
+    printf("7\n");
+    printf("8\n");
+    printf("9\n");
+    printf("10\n");
+}
+
+
+
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    char nombre[] = "Tomas";
+    char apellido[] = "Borghi";
+    int edad = 21;
+        
+    printf("%s\n", nombre);
+    printf("%s\n", apellido);
+    printf("%d\n", edad);
+    
+    return 0;
+}
