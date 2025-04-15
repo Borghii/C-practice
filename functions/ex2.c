6e609500d1d69a8b715f02bc758e867f02f77748
bichi <tomasborghi13@gmail.com>
2025-04-15 15:15:15 -0300

    modified:   .vscode/tasks.json

--- functions/ex2.c
+++ functions/ex2.c
@@ -1,100 +1,100 @@
 #include <stdio.h>
 
 void printMenu();
-
+int actualizarEnergia(int* energia, int consumo);
 void realizarAccion(int* energia, int opcion);
-
 void recolectarFruta(int* energia);
 void cargarBateria(int* energia);
 void esperar(int* energia);
 void apagarRobot(int* energia);
-
 void mostrarEnergia(int* energia);
 
-
-
-int main()
-{
-    int energia=100;
-
-
+int main() {
+    int energia = 100;
     int opcion;
     
-    do
-    {
+    do {
         printMenu();
-
-        scanf("%d",&opcion);
-
+        scanf("%d", &opcion);
         realizarAccion(&energia, opcion);
-
         mostrarEnergia(&energia);
-       
-    } while (energia>0);
+    } while (energia > 0);
     
-
-
-
-
-
+    printf("Robot apagado. Fin del programa.\n");
     return 0;
 }
 
-void printMenu(){
-    printf("Menu de opciones \n");
+void printMenu() {
+    printf("\nMenu de opciones\n");
     printf("1 - Recolectar fruta\n");
     printf("2 - Cargar bateria\n");
     printf("3 - Esperar\n");
     printf("4 - Apagar robot\n");
-
+    printf("Ingrese una opcion: ");
 }
 
-void realizarAccion(int* energia, int opcion){
-    switch (opcion)
-    {
-    case 1:
-        recolectarFruta(energia);
-    break;
-    case 2:
-        cargarBateria(energia);
-    break;
-    case 3:
-         esperar(energia);
-    break;
-    case 4:
-        apagarRobot(energia);
-    break;
-
-    default:
-    printf("Opcionno valida\n");
-
-        break;
+int actualizarEnergia(int* energia, int consumo) {
+    if (consumo < 0) {
+        if (*energia + consumo <= 0) {
+            printf("No hay suficiente energia para realizar esta accion.\n");
+            return 0;
+        }
     }
-}
-
-void recolectarFruta(int* energia){
-    *energia = *energia - 20;
-}
-
-void cargarBateria(int* energia){
-    if (*energia <= 70)
-    {
-        *energia += 30;
-    } else {
+    
+    *energia += consumo;
+    
+    if (*energia > 100) {
         *energia = 100;
     }
+    
+    return 1;
 }
 
-void esperar(int* energia){
-    *energia-=20;
+void realizarAccion(int* energia, int opcion) {
+    switch (opcion) {
+        case 1:
+            recolectarFruta(energia);
+            break;
+        case 2:
+            cargarBateria(energia);
+            break;
+        case 3:
+            esperar(energia);
+            break;
+        case 4:
+            apagarRobot(energia);
+            break;
+        default:
+            printf("Opcion no valida\n");
+            break;
+    }
 }
 
-void apagarRobot(int* energia){
-    *energia=0;
+void recolectarFruta(int* energia) {
+    printf("Intentando recolectar fruta...\n");
+    if (actualizarEnergia(energia, -20)) {
+        printf("Fruta recolectada con exito!\n");
+    }
 }
 
-void mostrarEnergia(int* energia){
-    printf("Energía actual: %d\n", *energia);
+void cargarBateria(int* energia) {
+    printf("Cargando bateria...\n");
+    actualizarEnergia(energia, 30);
+    printf("Bateria cargada!\n");
 }
 
+void esperar(int* energia) {
+    printf("El robot esta esperando...\n");
+    if (actualizarEnergia(energia, -10)) {
+        printf("Espera completada.\n");
+    }
+}
 
+void apagarRobot(int* energia) {
+    printf("Apagando robot...\n");
+    *energia = 0;
+}
+
+void mostrarEnergia(int* energia) {
+    printf("Energia actual: %d%%\n", *energia);
+}
